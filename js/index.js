c31faad6f185bddb678c5b5c2a82094a9854aeb2
Natalya <m_d.r.lecter@protonmail.com>
2024-03-23 20:45:44 +0400

    api youtube and css fix

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ const fetchTrendingVideos = async () => {
         url.searchParams.append('part', 'contentDetails,id,snippet');
         url.searchParams.append('chart', 'mostPopular');
         url.searchParams.append('regionCode', 'US');
+        url.searchParams.append('maxResults', '12');
         url.searchParams.append('key', API_KEY);
         
         const response = await fetch(url);
@@ -20,7 +21,38 @@ const fetchTrendingVideos = async () => {
     }
 }
 
-fetchTrendingVideos().then(videos => {
-    console.log(videos);
-});
+const displayVideo = async (videos) => {
+    videoListItems.textContent = '';
+
+    const listVideos = videos.items.map((video) => {
+        const li = document.createElement('li');
+        li.classList.add('video-list__item');
+        li.innerHTML = `
+        <article class="video-card">
+            <a href="/video.html?id=${video.id}" class="video-card__link">
+                <img src="${
+                    video.snippet.thumbnails.standard?.url || 
+                    video.snippet.thumbnails.high?.url
+                }" alt="video preview ${video.snippet.title}"
+                    class="video-card__thumbnail">
+                <h3 class="video-card__title">${video.snippet.title}</h3>
+                <p class="video-card__channel">${video.snippet.channelTitle}</p>
+                <p class="video-card__duration">${video.contentDetails.duration}</p>
+            </a>
+            <button class="video-card__favorite" type="button"
+                aria-label="Add to Favorites ${video.snippet.title}">
+                <svg class="video-card__icon">
+                    <use class="star-o" xlink:href="/image/sprite.svg#star-ob"></use>
+                    <use class="star" xlink:href="/image/sprite.svg#star"></use>
+                </svg>
+            </button>
+        </article>
+        `;
+        return li;
+    });
+
+    videoListItems.append(...listVideos);
+}
+
+fetchTrendingVideos().then(displayVideo);
 
